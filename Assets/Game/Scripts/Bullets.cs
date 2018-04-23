d8a5c7a18629e4c96f449641d760752de1bf5c2d
David <davidjejans@hotmail.nl>
2018-04-23 14:36:37 +0200

    I really didnt change anything

--- Assets/Game/Scripts/Bullets.cs
+++ Assets/Game/Scripts/Bullets.cs
@@ -3,26 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullets : MonoBehaviour {
+    [SerializeField] float m_Damage;
+
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Enemy"))
         {
-            
+            other.GetComponent<EnemyHealth>().TakeDamage(m_Damage);
         }
         DestroyObject(this.gameObject);
     }
 
     private void Start()
     {
-        if (PlayerStats.m_PoisonBullets == true)
-        {
-            gameObject.GetComponent<Renderer>().material.color = Color.green;
-        }
-    }
-
-    private void Update()
-    {
-       
         if (PlayerStats.m_PoisonBullets == true)
         {
             gameObject.GetComponent<Renderer>().material.color = Color.green;
---
+++ Assets/Game/Scripts/EnemyMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour {
+    [SerializeField] Transform m_PostOne;
+    [SerializeField] Transform m_PostTwo;
+    [SerializeField] Transform m_PlayerLocation;
+    [SerializeField] Transform m_GuardLocation;
+    [SerializeField] [Range(5f, 30f)] private float m_RayLength;
+    private Ray ray;
+    private RaycastHit hit;
+    private Vector3 m_Direction;
+    private NavMeshAgent m_NavMesh;
+    private int m_Location;
+    private string m_TargetName;
+    //[SerializeField] GameObject m_PostThree;
+
+    private int m_GoTo = 0;
+    // Use this for initialization
+    void Start () {
+        m_NavMesh = GetComponent<NavMeshAgent>();
+        m_NavMesh.SetDestination(m_PostOne.position);
+        m_TargetName = m_PlayerLocation.name;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_Direction = (m_PlayerLocation.position - m_GuardLocation.position).normalized;
+
+        if (Physics.Raycast(m_GuardLocation.position, m_Direction, out hit, m_RayLength))
+        {
+            if (hit.transform.name == m_TargetName)
+            {
+                Debug.DrawLine(m_GuardLocation.position, m_PlayerLocation.position, Color.red);
+            }
+            else if (hit.transform.name == "Obstacle")
+            {
+                Debug.DrawLine(m_GuardLocation.position, m_PlayerLocation.position, Color.grey);
+            }
+
+            if (hit.transform.name == m_TargetName)
+            {
+                m_NavMesh.SetDestination(m_PlayerLocation.position);
+            }
+            else if (m_Location == 1)
+            {
+                m_NavMesh.SetDestination(m_PostOne.position);
+            }
+            else if (m_Location == 2)
+            {
+                m_NavMesh.SetDestination(m_PostTwo.position);
+            }
+        }
+        else
+        {
+            Debug.DrawLine(m_GuardLocation.position, m_PlayerLocation.position, Color.green);
+        }
+       
+
+
+        
+        
+
+        if (m_GuardLocation.position.x >= m_PostOne.position.x -3 && m_GuardLocation.position.x <= m_PostOne.position.x +3 && m_GuardLocation.position.z >= m_PostOne.position.z - 3 && m_GuardLocation.position.z <= m_PostOne.position.z + 3)
+        {
+            m_Location = 2;
+        }
+        if (m_GuardLocation.position.x >= m_PostTwo.position.x - 3 && m_GuardLocation.position.x <= m_PostTwo.position.x +3 && m_GuardLocation.position.z >= m_PostTwo.position.z - 3 && m_GuardLocation.position.z <= m_PostTwo.position.z + 3)
+        {
+            m_Location = 1;
+        }
+        
+        
+    }
+}
