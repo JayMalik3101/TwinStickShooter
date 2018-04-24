cd21e2f4a42049754daba091b1d8b6fda46d4398
Unknown <davidjejans@hotmail.nl>
2018-04-24 11:51:13 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/CircleHealthBar.cs
+++ Assets/CircleHealthBar.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CircleHealthBar : MonoBehaviour {
-    public Image m_Bar;
-    public RectTransform m_Button;
+    [SerializeField] private Image m_Bar;
+    //public RectTransform m_Button; red knob 
+
 
     public float m_HealthValue = 0;
-
-	// Update is called once per frame
+    
 	void Update () {
         HealthChange(m_HealthValue);
 	}
@@ -18,7 +18,9 @@ public class CircleHealthBar : MonoBehaviour {
     {
         float amount = (healthvalue / 100.0f) * 180.0f / 360;
         m_Bar.fillAmount = amount;
+        /*
         float buttonAngle = amount * 360;
         m_Button.localEulerAngles = new Vector3(0, 0, -buttonAngle);
+        */
     }
 }
