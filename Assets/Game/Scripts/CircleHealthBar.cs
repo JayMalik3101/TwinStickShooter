d5b7de8a962a58b9486e89a87c42c02b9772a6ae
Unknown <davidjejans@hotmail.nl>
2018-04-24 12:39:26 +0200

    Enemy moves (not fully functional) Circular healthbar fully functional (Still need to implement being able to put it on whatever you want)

--- Assets/Game/Scripts/CircleHealthBar.cs
+++ Assets/Game/Scripts/CircleHealthBar.cs
@@ -8,16 +8,17 @@ public class CircleHealthBar : MonoBehaviour {
     //public RectTransform m_Button; red knob 
 
 
-    public float m_HealthValue = 0;
-    
+    public float m_HealthValue;
+    public float m_MaxHealth;
 	void Update () {
         HealthChange(m_HealthValue);
 	}
 
     private void HealthChange(float healthvalue)
     {
-        float amount = (healthvalue / 100.0f) * 180.0f / 360;
-        m_Bar.fillAmount = amount;
+        float amount = (healthvalue / m_MaxHealth);
+        
+        m_Bar.fillAmount = amount / 2f;
         /*
         float buttonAngle = amount * 360;
         m_Button.localEulerAngles = new Vector3(0, 0, -buttonAngle);
