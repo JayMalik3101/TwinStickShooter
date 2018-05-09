07ddaa50457df55cb31da7053eac5558bfb284fc
Unknown <stoktogoelova@gmail.com>
2018-05-09 14:32:10 +0200

    Quit button works

---
+++ Assets/Game/Scripts/UI/UIButtons.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIButtons : MonoBehaviour
+{
+
+    public void Iwanttodie()
+    {
+        Application.Quit();
+        Debug.Log("I want to die)");
+    }
+}
