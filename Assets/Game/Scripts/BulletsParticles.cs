f1a329429035a29420cc25fa1f7113f61b66b52a
Unknown <stoktogoelova@gmail.com>
2018-05-13 20:04:40 +0200

    B ulletswerken nu
    YE s

---
+++ Assets/Game/Scripts/BulletsParticles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletsParticles : MonoBehaviour {
+    [SerializeField] private float m_Damage;
+    [SerializeField] private float m_BulletLifetime;
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<EnemyHealth>().TakeDamage(m_Damage);
+        }
+    }
+
+    private void Update()
+    {
+        m_BulletLifetime -= Time.deltaTime;
+        if (m_BulletLifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
