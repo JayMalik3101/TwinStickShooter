35c3b280fd2cd2cbccfd180b3d9f59eabd99f215
Unknown <stoktogoelova@gmail.com>
2018-05-16 15:07:21 +0200

    les van david senpai

--- Assets/Game/Scripts/SingleShotFire.cs
+++ Assets/Game/Scripts/SingleShotFire.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SingleShotFire : MonoBehaviour {
     [SerializeField] private float m_InitialForce = 1500;
@@ -9,14 +10,19 @@ public class SingleShotFire : MonoBehaviour {
     [SerializeField] private float m_OriginalFireRate;
     [SerializeField] private float m_OriginalReloadTime;
     [SerializeField] private float m_MagSize;
+
     public float m_CurrentReloadTime;
     public float m_FireRate;
     public float m_CurrentMagSize;
     public bool m_CurrentlyReloading;
+
+    private Text m_AmmoCount;
     private void Start()
     {
+        m_AmmoCount = GameObject.Find("AmmoCount").GetComponent<Text>();
         m_CurrentMagSize = m_MagSize;
         m_FireRate = m_OriginalFireRate;
+        m_AmmoCount.text = m_CurrentMagSize + "/" + m_MagSize;
     }
     private void Update()
     {
@@ -34,6 +40,7 @@ public class SingleShotFire : MonoBehaviour {
                 m_CurrentMagSize = m_MagSize;
                 m_CurrentReloadTime = m_OriginalReloadTime;
                 m_CurrentlyReloading = false;
+                m_AmmoCount.text = m_CurrentMagSize + "/" + m_MagSize;
             }
         }
         if (Input.GetMouseButton(0) && m_FireRate <= 0 && m_CurrentlyReloading == false && m_CurrentMagSize >0)
@@ -43,7 +50,10 @@ public class SingleShotFire : MonoBehaviour {
             newProjectile.GetComponent<Renderer>().material.color = GetComponentInParent<PlayerStats>().m_PoisonBullets ? Color.green : Color.white; 
             newProjectile.AddForce(transform.forward * m_InitialForce, ForceMode.Impulse);
             m_FireRate = m_OriginalFireRate;
+
+            m_AmmoCount.text = m_CurrentMagSize + "/" + m_MagSize;
         }
+
         
     }
 }
