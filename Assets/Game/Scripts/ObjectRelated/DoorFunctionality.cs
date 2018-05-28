18bc23ecda60ac9081c761ce342ce62f511f45a0
Unknown <davidjejans@hotmail.nl>
2018-05-28 14:21:12 +0200

    All sort of stuff

--- Assets/Game/Scripts/ObjectRelated/DoorFunctionality.cs
+++ Assets/Game/Scripts/ObjectRelated/DoorFunctionality.cs
@@ -2,17 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorFunctionality : MonoBehaviour {
+public class DoorFunctionality : MonoBehaviour
+{
     [SerializeField] private bool m_IsLocked;
-    private bool m_DoorIsOpen;
+    private bool m_DoorIsOpen = false;
     private Animator m_DoorAnimator;
+    private PlayerStats m_PlayerStats;
+    public bool DoorIsOpen
+    {
+        set
+        {
+            m_DoorIsOpen = value;
+            if (value == true)
+                m_DoorAnimator.SetTrigger("Activate");
+            else
+                m_DoorAnimator.SetTrigger("Deactivate");
+        }
+    }
+    void Start()
+    {
+        m_DoorAnimator = GetComponent<Animator>();
+        m_PlayerStats = GameObject.Find("Player").GetComponent<PlayerStats>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) == true && other.CompareTag("Player"))
+        {
+            if (m_IsLocked == true && m_PlayerStats.m_CurrentKeyCards > 0)
+            {
+                m_PlayerStats.m_CurrentKeyCards -= 1;
+                m_IsLocked = false;
+                if (m_DoorIsOpen == false)
+                {
+                    DoorIsOpen = true;
+                    m_DoorIsOpen = true;
+                }
+                else if (m_DoorIsOpen == true)
+                {
+                    DoorIsOpen = false;
+                    m_DoorIsOpen = false;
+                }
+            }
+            else if (m_IsLocked == false)
+            {
+                if (m_DoorIsOpen == false)
+                {
+                    DoorIsOpen = true;
+                    m_DoorIsOpen = true;
+                }
+                else if (m_DoorIsOpen == true)
+                {
+                    DoorIsOpen = false;
+                    m_DoorIsOpen = false;
+                }
+            }
 
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        }
+    }
 }
