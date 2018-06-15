ab403cbca1b1ca20316619149d62bbf2a253b228
Unknown <davidjejans@hotmail.nl>
2018-06-15 12:29:24 +0200

    hiteffect and destroying particlesystems with lifetime

--- Assets/Game/Scripts/Bullets.cs
+++ Assets/Game/Scripts/Bullets.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour {
     [SerializeField] float m_Damage;
+    [SerializeField] GameObject m_HitEffect;
     public string m_Owner;
 
     private void OnTriggerEnter(Collider other)
@@ -11,6 +12,7 @@ public class Bullets : MonoBehaviour {
         if (other.CompareTag("Enemy"))
         {
             other.GetComponent<EnemyHealth>().TakeDamage(m_Damage);
+            GameObject HitEffect = Instantiate(m_HitEffect, transform.position, new Quaternion(0, 0, 0, 0));
         }
         else if (other.CompareTag("Player"))
         {
---
+++ Assets/Game/Scripts/ObjectRelated/DestroyOnComplete.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnComplete : MonoBehaviour {
+    [SerializeField] private float m_DestroyInThisTime;
+	// Update is called once per frame
+	void Update () {
+        m_DestroyInThisTime -= Time.deltaTime;
+        if (m_DestroyInThisTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
