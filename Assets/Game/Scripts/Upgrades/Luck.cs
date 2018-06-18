7b3ecc32a8e022d663375a9739fe69d2d96cd1ad
Unknown <davidjejans@hotmail.nl>
2018-06-18 10:26:46 +0200

    upgrade level showcasing

--- Assets/Game/Scripts/Upgrades/Luck.cs
+++ Assets/Game/Scripts/Upgrades/Luck.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Luck : MonoBehaviour {
     [SerializeField] private float m_UpgradeCost;
     [SerializeField] private int m_MaxLevel;
+    [SerializeField] private Text m_UpgradeText;
+    private void Start()
+    {
+        LevelDisplay();
+    }
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown(KeyCode.E) == true && other.CompareTag("Player"))
@@ -18,12 +24,19 @@ public class Luck : MonoBehaviour {
             {
                 // input not having enough money here
             }
-
+            LevelDisplay()
         }
     }
     private void UpgradeEffect(Collider other)
     {
         StatManager.m_Data.m_LuckModifier += 1f;
         StatManager.m_Data.m_LuckLevel += 1;
+    }
+    private void LevelDisplay()
+    {
+        if (m_UpgradeText != null)
+        {
+            m_UpgradeText.text = StatManager.m_Data.m_LuckLevel + " / " + m_MaxLevel;
+        }
     }
 }
--- Assets/Game/Scripts/Upgrades/MaxHealth.cs
+++ Assets/Game/Scripts/Upgrades/MaxHealth.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MaxHealth : MonoBehaviour {
     [SerializeField] private float m_UpgradeCost;
     [SerializeField] private int m_MaxLevel;
+    [SerializeField] private Text m_UpgradeText;
+    private void Start()
+    {
+        LevelDisplay();
+    }
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown(KeyCode.E) == true && other.CompareTag("Player"))
@@ -18,12 +24,19 @@ public class MaxHealth : MonoBehaviour {
             {
                 // input not having enough money here
             }
-
+            LevelDisplay()
         }
     }
     private void UpgradeEffect(Collider other)
     {
         StatManager.m_Data.m_MaxHealthModifier += 10;
         StatManager.m_Data.m_HealthLevel += 1;
+    }
+    private void LevelDisplay()
+    {
+        if (m_UpgradeText != null)
+        {
+            m_UpgradeText.text = StatManager.m_Data.m_HealthLevel + " / " + m_MaxLevel;
+        }
     }
 }
