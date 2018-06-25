553ffc423a695bd2d5cb9be5aa710867f64e1c27
Unknown <davidjejans@hotmail.nl>
2018-06-25 15:09:42 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/LoadingScreens.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreens : MonoBehaviour {
+    [SerializeField] private List<Sprite> m_LoadingScreens;
+    private int currentimage;
+
+	// Use this for initialization
+	void Start () {
+        currentimage = Mathf.RoundToInt(Random.Range(0, m_LoadingScreens.Count));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
